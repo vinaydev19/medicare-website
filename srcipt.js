61be1828b6672330222e46242a79ac8b7d93589d
vinay1945 <vinaykumarporandla19@gmail.com>
2024-12-31 02:21:42 +0530

    add contact us working functionality

--- srcipt.js
+++ srcipt.js
@@ -52,3 +52,49 @@ slider.addEventListener("mouseenter", stopAutoSlide);
 slider.addEventListener("mouseleave", startAutoSlide);
 
 startAutoSlide();
+
+function sendMail() {
+
+
+
+
+  // Fetching values inside the function
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const city = document.getElementById("city").value;
+  const phone = document.getElementById("phone").value;
+  const message = document.getElementById("message").value;
+
+  var params = {
+    name: name,
+    email: email,
+    city: city,
+    phone: phone,
+    message: message,
+  };
+
+  const serviceID = "service_l02vxnp";
+  const templateID = "template_b3qk2qr"; // Correct template ID here
+
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+      // Clear input fields after success
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+      document.getElementById("city").value = "";
+      document.getElementById("phone").value = "";
+      console.log(res);
+      alert("Your message sent successfully!!");
+    })
+    .catch((err) => console.log(err));
+}
+
+// Adding event listener
+const SentFormBtn = document.getElementById("SentFormBtn");
+
+SentFormBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  sendMail();
+});
