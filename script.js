32dc32c8a1f5ae63f4d780bf32ddbd5ff823b901
vinay1945 <vinaykumarporandla19@gmail.com>
2025-02-09 21:19:35 +0530

    client changes

--- script.js
+++ script.js
@@ -13,29 +13,50 @@ openbtn.addEventListener("click", () => {
   openbtn.classList.add("hide");
   closebtn.classList.remove("hide");
 });
-let currentIndex = 0;
-const cardWidth = 500; 
-const visibleCards = 3;
-const totalCards = document.querySelectorAll(".testimonial-card").length;
 
-function updateTestimonials() {
-  const offset = -currentIndex * cardWidth;
-  container.style.transform = `translateX(${offset}px)`;
+// let currentIndex = 0;
+// const cardWidth = 500;
+// const visibleCards = 3;
+// const totalCards = document.querySelectorAll(".testimonial-card").length;
+
+// function updateTestimonials() {
+//   const offset = -currentIndex * cardWidth;
+//   container.style.transform = `translateX(${offset}px)`;
+// }
+
+// prevBtn.addEventListener("click", () => {
+//   if (currentIndex > 0) {
+//     currentIndex--;
+//     updateTestimonials();
+//   }
+// });
+
+// nextBtn.addEventListener("click", () => {
+//   if (currentIndex < totalCards - visibleCards) {
+//     currentIndex++;
+//     updateTestimonials();
+//   }
+// });
+
+// updateTestimonials();
+
+function sendMail() {
+  var params = {
+    full_name: document.getElementById("fullName").value,
+    email: document.getElementById("Email-id").value,
+    phone: document.getElementById("Phone-No").value,
+    city: document.getElementById("City").value,
+    requirements: document.getElementById("Requirements").value,
+  };
+
+  const serviceID = "service_vold2ul";
+  const templateID = "template_imi6jyh";
+
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+      console.log(res);
+      alert("Your message sent successfully!!");
+    })
+    .catch((err) => console.log(err));
 }
-
-prevBtn.addEventListener("click", () => {
-  if (currentIndex > 0) {
-    currentIndex--;
-    updateTestimonials();
-  }
-});
-
-nextBtn.addEventListener("click", () => {
-  if (currentIndex < totalCards - visibleCards) {
-    currentIndex++;
-    updateTestimonials();
-  }
-});
-
-
-updateTestimonials();
